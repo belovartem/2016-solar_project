218f3e384667fbf4db4d1cc640d907b2a9e14f36
islambek243 <55623811+islambek243@users.noreply.github.com>
2019-12-19 11:40:06 +0300

    I did part of code

--- solar_input.py
+++ solar_input.py
@@ -23,6 +23,10 @@ def read_space_objects_data_from_file(input_filename):
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            elif object_type == "planet":  # FIXME: do the same for planet
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
             else:
                 print("Unknown space object")
 
@@ -43,6 +47,14 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
+    parameters = line.split()
+    star.R = int(parameters[1])
+    star.color = parameters[2]
+    star.m = float(parameters[3])
+    star.x = float(parameters[4])
+    star.y = float(parameters[5])
+    star.Vx = float(parameters[6])
+    star.Vy = float(parameters[7])
 
     pass  # FIXME: not done yet
 
@@ -58,6 +70,17 @@ def parse_planet_parameters(line, planet):
 
     Параметры:
 
+    **line** — строка с описание планеты.
+    **planet** — объект планеты.
+    """
+    parameters = line.split()
+    planet.R = int(parameters[1])
+    planet.color = parameters[2]
+    planet.m = float(parameters[3])
+    planet.x = float(parameters[4])
+    planet.y = float(parameters[5])
+    planet.Vx = float(parameters[6])
+    planet.Vy = float(parameters[7])
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
@@ -77,10 +100,17 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
+            print(obj.R, obj.color, obj.m, obj.x, obj.y, obj.Vx, obj.Vy, file=out_file)
             # FIXME: should store real values
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
+r = open('statistics.txt', 'a') #record file
+def click_button(): #record button function
+	r.write(str(q) + ' ' + str(w) + '\n')
+	r.close()
+btn = Button(text="save statistics", background="#555", foreground="#ccc",
+             padx="20", pady="8", font="16", command=click_button) #record button
 
+btn.pack()
 if __name__ == "__main__":
     print("This module is not for direct call!")
